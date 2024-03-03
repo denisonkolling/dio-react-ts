d3dda8523a62af2ae4287e32acd54d28dd245179
Denison Kolling <denisonkolling@gmail.com>
2024-03-03 07:52:08 -0300

    update header with user auth profile image

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Signin from './pages/signin';
 import Signup from './pages/signup';
+import { Feed } from './pages/feed';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Route path="/" element={<Home />} />
 			<Route path="/signin" element={<Signin />} />
 			<Route path="/signup" element={<Signup />} />
+			<Route path="/feed" element={<Feed />} />
 		</Routes>
 	);
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,31 +6,48 @@ import {
 	Menu,
 	MenuRight,
 	Row,
+	UserPicture
 } from './styles';
 import Button from '../Button';
 import img from '../../assets/logo-dio.png';
 import { useNavigate } from 'react-router-dom';
+import { IHeader } from './types';
 
-const Header = () => {
-
+const Header = ({ autenticado }: IHeader) => {
 	const navigate = useNavigate();
-
 
 	return (
 		<Wrapper>
 			<Container>
 				<Row>
 					<img src={img} alt="logo" />
-					<BuscarInputContainer>
-						<Input placeholder="Buscar..." />
-					</BuscarInputContainer>
-					<Menu>Live Code</Menu>
-					<Menu>Global</Menu>
+					{autenticado ? (
+						<>
+							<BuscarInputContainer>
+								<Input placeholder="Buscar..." />
+							</BuscarInputContainer>
+							<Menu>Live Code</Menu>
+							<Menu>Global</Menu>
+						</>
+					) : null}
 				</Row>
 				<Row>
+				{autenticado ? (
+                <UserPicture src="https://avatars.githubusercontent.com/u/45184516?v=4"/>
+              ) : (
+					<>
 					<MenuRight href="#">Home</MenuRight>
-					<Button title="Entrar" variant={''} onClick={() => navigate("/signin")} />
-					<Button title="Cadastrar-se" variant={''} onClick={() => navigate("/signup")}/>
+					<Button
+						title="Entrar"
+						variant={''}
+						onClick={() => navigate('/signin')}
+					/>
+					<Button
+						title="Cadastrar-se"
+						variant={''}
+						onClick={() => navigate('/signup')}
+					/>
+					</>)}
 				</Row>
 			</Container>
 		</Wrapper>
